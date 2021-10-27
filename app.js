4df9fd5b7c1c71cbb5b549bc98640df72f00b645
jaehun0712 <jaehun07@naver.com>
2021-10-27 16:47:12 +0900

    1

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 var express = require('express');
+
+const cors = require('cors');
+
 var app = express();
+
+app.use(cors()); //cors 미들웨어 
 
 //api 서비스를 할 배열
 const contents = [
     {
         id:1,
-        nm:'자동차11'
+        name:'html 공부하기',
+        done:true
     },
     {
         id:2,
-        nm:'신계약'
+        name:'CSS 공부하기',
+        done:true
+    },
+    {
+        id:3,
+        name:'Python 공부하기',
+        done:false
+    },
+    {
+        id:4,
+        name:'Java 공부하기',
+        done:false
     }
 ]
 
@@ -19,5 +36,11 @@ app.get('/', function(req, res) {
   res.json(contents);
 });
 
+app.get('/add', function(req, res) {
+    console.log("add");
+    contents.push({id:contents.length+1 , name: req.query.name , done :false});
+    res.json(contents);
+  });
+
 //express 웹서버 리스닝
-app.listen(5000, () => console.log('Listening on port 5000'));
+app.listen(3000, () => console.log('Listening on port 3000'));
