75ac1ebfec272ef3e702490d1253498cca668e16
jaehun0712 <jaehun07@naver.com>
2021-12-07 18:57:28 +0900

    ddddd

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-var express = require('express');
-
+const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql');
 
 var app = express();
 
 app.use(cors()); //cors 미들웨어 
+
+ 
+const connection = mysql.createConnection({
+    host: '3.38.104.18',
+    user: 'jaehun',
+    password: 'Welcome!1',
+    database: 'todo'
+  });
+
+connection.connect();
+
+connection.query('SELECT * FROM TODO_LIST', function (error, results, fields) {
+    if (error) {
+        console.log(error);
+    }
+    console.log('result1',results);
+});
+    
+
 
 //api 서비스를 할 배열
 const contents = [
@@ -31,16 +50,30 @@ const contents = [
 ] 
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  //res.send('hello world');
-  res.json(contents);
+//app.get('/', function(req, res) {
+  //res.json(contents);
+//});
+app.get('/', (req, res) => {
+    connection.query('SELECT * from TODO_LIST', (error, rows) => {
+      if (error) throw error;
+      res.send(rows);
+    });
 });
-
+//app.get('/add', function(req, res) {
+    //console.log("add");
+    //contents.push({id:contents.length+1 , name: req.query.name , done :false});
+    //res.json(contents);
+//  });
 app.get('/add', function(req, res) {
-    console.log("add");
-    contents.push({id:contents.length+1 , name: req.query.name , done :false});
-    res.json(contents);
+    var sql = 'INSERT INTO TODO_LIST (name,done) VALUES (?,?)'
+    var param = [req.query.name, 'N'];
+    connection.query(sql, param, (error, rows) => {
+        if (error) throw error;
+        res.send(rows);
+      });
   });
+
+connection.end();
 
 //express 웹서버 리스닝
 app.listen(3000, () => console.log('Listening on port 3000'));
